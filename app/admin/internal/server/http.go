f9d151e3ac5f7d2d2daa57a27f861d037d08958e
kiri <kiripeng@outlook.com>
2021-07-14 23:19:19 +0800

    添加

---
+++ app/admin/internal/server/http.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	v1 "ki-book/api/helloworld/v1"
+	conf2 "ki-book/app/admin/internal/conf"
+	service2 "ki-book/app/admin/internal/service"
+)
+
+// NewHTTPServer new a HTTP server.
+func NewHTTPServer(c *conf2.Server, greeter *service2.GreeterService, logger log.Logger) *http.Server {
+	var opts = []http.ServerOption{
+		http.Middleware(
+			recovery.Recovery(),
+			tracing.Server(),
+			logging.Server(logger),
+			metrics.Server(),
+			validate.Validator(),
+		),
+	}
+	if c.Http.Network != "" {
+		opts = append(opts, http.Network(c.Http.Network))
+	}
+	if c.Http.Addr != "" {
+		opts = append(opts, http.Address(c.Http.Addr))
+	}
+	if c.Http.Timeout != nil {
+		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
+	}
+	srv := http.NewServer(opts...)
+	v1.RegisterGreeterHTTPServer(srv, greeter)
+	return srv
+}
